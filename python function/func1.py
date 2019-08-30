3a663b1a668fb801aff0476c17a25293e9cdf25e
hhj <hhj_happy@163.com>
2019-08-30 10:21:02 +0800

    add new file

---
+++ python function/func1.py
@@ -0,0 +1,7 @@
+def cheese_and crackers(cheese_count,boxe_of_crackers):
+    print("You have %d chesses!" %cheese_count)
+    print("You have %d boxe of crackeers!" %boxe_of_crackers)
+    print("Man that's enough for a party!")
+    print("Get a blanket.")
+
+print("We can just give th function numbers directly")
